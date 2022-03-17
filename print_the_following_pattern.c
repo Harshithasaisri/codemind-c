83ccb393aae6493d85861b4f1a0c12621adcf527
Harshithasaisri <96580368+Harshithasaisri@users.noreply.github.com>
2022-03-17 11:50:49 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {            if(j==1||i==n||i==j)            {                printf("*");            }            else            {                printf(" ");            }         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -3,11 +3,18 @@ int main()
 {
     int n,i,j;
     scanf("%d",&n);
-    for(i=n;i>=1;i--)
+    for(i=1;i<=n;i++)
     {
-        for(j=i;j>=1;j--)
+        for(j=1;j<=n;j++)
         {
-            printf("%c ",i+64);
+           if(j==1||i==n||i==j)
+           {
+               printf("*");
+           }
+           else
+           {
+               printf(" ");
+           }
         }
         printf("
 ");
