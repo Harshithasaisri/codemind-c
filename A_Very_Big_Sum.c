0eea15f150cb730cf74fa6a1e1cda73b3b5c1bd5
Harshithasaisri <96580368+Harshithasaisri@users.noreply.github.com>
2022-04-27 09:15:55 +0530

    #include <stdio.h> int main() {     int t,i;    scanf("%d",&t);     long int arr[1000];     long int s=0;     for(i=0;i<t;i++)         {         scanf("%ld",&arr[i]);     }     for(i=0;i<t;i++)         {         s=s+arr[i];     }     printf("%ld",s);     return 0; }

---
+++ A_Very_Big_Sum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int t,i;
+   scanf("%d",&t);
+    long int arr[1000];
+    long int s=0;
+    for(i=0;i<t;i++)
+        {
+        scanf("%ld",&arr[i]);
+    }
+    for(i=0;i<t;i++)
+        {
+        s=s+arr[i];
+    }
+    printf("%ld",s);   
+    return 0;
+}
